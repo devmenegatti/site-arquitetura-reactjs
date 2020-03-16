30509a94e1490aa0831097b9edd274b55a788a29
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-16 10:21:33 -0300

    Estilos globais da aplicaçao e definiçao de rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Site Portifolio</h1>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
