27d5f70bb03bc1b173d0eefc76dd2c92916fcc49
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-31 23:08:18 -0300

    Criação, estilizaçãodae e conexão api para obetenção de dados na pagina de projetos

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
-// import { Container } from './styles';
+import api from '../../services/api';
+import SlideShow from '../../components/SlideShow';
+
+import {
+  Container
+} from './styles';
 
 export default function Project() {
   let { id } = useParams();
+  const [images, setImages] = useState([]);
+
+
+  useEffect(()=>{
+    loadImages();
+  },[id]);
+
+  async function loadImages() {
+    await api.get(`/project\?category=${id}`).then( response => {
+      setImages(response.data);
+    }).catch(error => {
+      alert(error);
+    });
+  }
   return (
-    <div>
-      <h1>
-        {id}
-      </h1>
-    </div>
+    <Container>
+      {console.info(images)}
+      {(images.length) ? <SlideShow images={images} /> : <span>Não há projetos disponiveis</span>}
+    </Container>
   );
 }
---
+++ src/pages/Project/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 5px 0px 20px 0px;
+  margin-bottom: 15px;
+`;
